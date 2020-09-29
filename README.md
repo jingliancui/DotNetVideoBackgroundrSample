cacefa338a5c887c97594ae3ed1a108e77723671
jingliancui <ericcui@qq.com>
2020-09-29 21:51:01 +0800

    add sample code
    add sample code

---
+++ README.md
@@ -0,0 +1,14 @@
+# XamarinFormsVideoBackgroundrSample
+Sample for showing video background on Xamarin.Forms
+
+Tutorial:
+
+
+
+QQ Group:313308215
+
+WeChat Official Account(微信公众号):
+
+Xamarin Library
+
+<img src="https://raw.githubusercontent.com/jingliancui/XamarinFormsMicrosoftAppCenterSample/master/Images/wechatqrcode.jpg"/>
---
+++ VideoBackgroundSample/SampleApp/SampleApp/SampleApp/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            SizeChanged += MainPage_SizeChanged;
+        }
+
+        private void MainPage_SizeChanged(object sender, EventArgs e)
+        {
+            Player.Aspect = Aspect.AspectFill;
+            Player.Source = "ms-appx:///video.mp4";
+            Player.Volume = 0;
+            Player.IsLooping = true;
+            Player.Play();
+        }
+    }
+}
